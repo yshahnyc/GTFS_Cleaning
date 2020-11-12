c9131e917d38cbd6b8cd7a8f5c62613e027a2735
YS <yshahnyc@users.noreply.github.com>
2020-11-12 14:09:27 -0500

    Update Data_Pull.R

--- Data_Pull.R
+++ Data_Pull.R
@@ -1,7 +1,7 @@
 library(tidytransit)
 library(tidyverse)
 
-region_feed = feedlist %>%
+region_feed <- feedlist %>%
   filter(loc_n %in% c("Durham Regional Municipality",
                     "Hamilton",
                     "Burlington",
@@ -12,25 +12,25 @@ region_feed = feedlist %>%
                     "Toronto"))
 
 #load gtfs feed (using TTC as an example here)
-City_feed = region_feed %>%
+City_feed <- region_feed %>%
   filter(t %in% "TTC GTFS") %>%
   select(url_d)
 
-City_gtfs = read_gtfs(City_feed[1,])
+City_gtfs <- read_gtfs(City_feed[1,])
 
-City_freq = get_route_frequency(City_gtfs,
+City_freq <- get_route_frequency(City_gtfs,
                                 start_hour=22,
                                 end_hour=3)
 #coerce route_ids
-#City_freq$route_id = as.numeric(City_freq$route_id)
+#City_freq$route_id <- as.numeric(City_freq$route_id)
 
-City_route_id = City_gtfs$routes$route_id
-City_route_name = City_gtfs$routes$route_short_name
+City_route_id <- City_gtfs$routes$route_id
+City_route_name <- City_gtfs$routes$route_short_name
 
-City_routes = cbind.data.frame(City_route_id,City_route_name)
-colnames(City_routes) = c("route_id", "route_name")
+City_routes <- cbind.data.frame(City_route_id,City_route_name)
+colnames(City_routes) <- c("route_id", "route_name")
 
-City_freq = left_join(City_freq, City_routes, by = "route_id")
+City_freq <- left_join(City_freq, City_routes, by = "route_id")
 
 #City_freq %>%
 #  write_csv("")
